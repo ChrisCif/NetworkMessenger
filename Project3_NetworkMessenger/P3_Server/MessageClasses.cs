9f23bb1b5b4cda02273609b69ecad02bc905ea98
cmc5193 <cmc5193@g.rit.edu>
2018-04-16 22:51:37 -0400

    First Commit?

---
+++ Project3_NetworkMessenger/P3_Server/MessageClasses.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace P3_Server
+{
+
+    public static class Messages
+    {
+
+        public static List<Message> list = new List<Message>();
+
+        public static void Add(Message mes)
+        {
+            list.Add(mes);
+        }
+
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Message
+    {
+
+        [JsonProperty]
+        private string content;
+        [JsonProperty]
+        private string sender;
+        [JsonProperty]
+        private string channel;
+        [JsonProperty]
+        private DateTime timestamp;
+        
+        /*
+        private string content;
+        private User sender;
+        private Channel channel;
+        private DateTime timestamp;
+        private ulong id;
+        private int partID; // If the message is too long and needs to be broken up into parts (different messages)
+
+        public Message(string content, User sender, Channel channel, DateTime timestamp, ulong id, int partID)
+        {
+            this.content = content;
+            this.sender = sender;
+            this.channel = channel;
+            this.timestamp = timestamp;
+            this.id = id;
+            this.partID = partID;
+        }
+        */
+
+    }
+
+    public class User
+    {
+        private string username;
+        private ulong id;
+        private string password;
+
+        public User(string username, ulong id, string password)
+        {
+            this.username = username;
+            this.id = id;
+            this.password = password;
+        }
+    }
+
+    public class UserChannel
+    {
+        private User user;
+        private Channel channel;
+
+        public UserChannel(User user, Channel channel)
+        {
+            this.user = user;
+            this.channel = channel;
+        }
+    }
+
+    public class Channel
+    {
+        private ulong id;
+        private string name;
+        private User creator;
+
+        public Channel(ulong id, string name, User creator)
+        {
+            this.id = id;
+            this.name = name;
+            this.creator = creator;
+        }
+    }
+
+    public class UserChannelList
+    {
+
+        static List<UserChannel> list = new List<UserChannel>();
+
+        public static void Add(UserChannel uc)
+        {
+            list.Add(uc);
+        }
+
+    }
+
+}
---
+++ Project3_NetworkMessenger/P3_Server/SocketHandler.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Globalization;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace P3_Server
+{
+
+    public class SocketHandler
+    {
+        public const int BufferSize = 4096;
+
+        WebSocket socket;
+
+        SocketHandler(WebSocket socket)
+        {
+            this.socket = socket;
+        }
+
+        async Task ReceiveMessages()
+        {
+
+            var buffer = new byte[BufferSize];
+            var seg = new ArraySegment<byte>(buffer);
+
+            while (this.socket.State == WebSocketState.Open)
+            {
+                
+                var inbuffer = new byte[BufferSize];
+
+                await this.socket.ReceiveAsync(inbuffer, CancellationToken.None);
+                await ParseMessage(inbuffer);
+
+                Thread.Sleep(1000);
+
+                //await EchoMessages();
+
+            }
+
+        }
+
+        async Task ParseMessage(byte[] buffer)
+        {
+            
+            var sMess = System.Text.Encoding.Default.GetString(buffer);
+            var message = JsonConvert.DeserializeObject<Message>(sMess);
+            
+            Messages.Add(message);
+
+            var outbuffer = System.Text.Encoding.Default.GetBytes(JsonConvert.SerializeObject(message));
+            var outgoing = new ArraySegment<byte>(outbuffer, 0, outbuffer.Length);
+            await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+
+        }
+
+        async Task EchoMessages()
+        {
+
+            foreach(Message m in Messages.list)
+            {
+
+                var outbuffer = System.Text.Encoding.Default.GetBytes(JsonConvert.SerializeObject(m));
+                var outgoing = new ArraySegment<byte>(outbuffer, 0, outbuffer.Length);
+                await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+
+            }
+
+        }
+
+        static async Task Acceptor(HttpContext hc, Func<Task> n)
+        {
+            if (!hc.WebSockets.IsWebSocketRequest)
+            {
+                await hc.Response.WriteAsync("Not a web socket request");
+                return;
+            }
+
+            while (true)
+            {
+                var socket = await hc.WebSockets.AcceptWebSocketAsync();
+                var h = new SocketHandler(socket);
+                await h.ReceiveMessages();
+            }
+            
+        }
+
+        public static void Map(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.Use(SocketHandler.Acceptor);
+        }
+    }
+
+
+
+}
